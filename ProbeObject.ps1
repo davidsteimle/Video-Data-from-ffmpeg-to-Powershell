fd8e7020f420242237adfdf111e99072cd893f71
David Steimle <21266257+davidsteimle@users.noreply.github.com>
2019-11-05 16:42:10 -0500

    Update ProbeObject.ps1

--- ProbeObject.ps1
+++ ProbeObject.ps1
@@ -1,49 +1,128 @@
-ffprobe -i furiadeloskaratecas1983.mp4 | grep Duration
+param(
+    [string]$Filename
+)
 
+class video{
+    [string]$Filename
+    [string]$MajorBrand
+    [string]$MinorVersion
+    [string]$CompatibleBrands
+    [string]$CreationTime
+    [psobject]$Duration
+    [string]$Start
+    [string]$BitRate
+    [array]$VideoStream
+    [array]$AudioStream
+    [array]$HandlerName
+}
 
-  Id CommandLine
-  -- -----------
-   1 cat $PROFILE
-   2 $PROFILE
-   3 New-ScriptFileInfo -Path $PROFILE -Author "Steimle, David B." -Descripti…
-   4 New-ScriptFileInfo -Path $PROFILE -Author "Steimle, David B." -Descripti…
-   5 cat $PROFILE
-   6 vi $PROFILE
-   7 $PROFILE
-   8 $test = ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4
-   9 $test | Select-String Duration
-  10 $test | Select-String 'Duration'
-  11 $test = $test -split ("`n")
-  12 $test
-  13 $test = ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4
-  14 $test
-  15 $test = $(ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4)
-  16 $test
-  17 ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4
-  18 $test = Invoke-Command ffprobe -i /home/david/Videos/furiadeloskaratecas…
-  19 $test = Invoke-Expression "ffprobe -i /home/david/Videos/furiadeloskarat…
-  20 $test
-  21 bash ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4
-  22 $test = ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4 | Out-…
-  23 $test
-  24 $test = ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4 2>&1
-  25 $test
-  26 $test[0]
-  27 $test[1]
-  28 $test -contains "Duration"
-  29     class Time{…
-  30     }
-  31     class Time{…
-  32 [Time]$x = @(1,2,3,4)
-  33 $test.foreach({if($PSItem -match "Duration"){$PSItem}})
-  34 $x = $test.foreach({if($PSItem -match "Duration"){$PSItem}})
-  35 $x
-  36 $x -match "[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}"
-  37 $Matches
-  38 [regex]::Match($x,"[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}")
-  39 ([regex]::Match($x,"[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}")).Value
-  40 $y = ([regex]::Match($x,"[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}")).Value
-  41 $y = $y -replace '.',':'
-  42 $y
-  43 $y = ([regex]::Match($x,"[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}")).Value
-  44 $y = $y -replace '\.',':'
+# class time{
+#     [int]$Hours
+#     [int]$Minutes
+#     [int]$Seconds
+#     [int]$Milliseconds
+# }
+
+# Assigning ffprobe information to variable via redirection is required in bash systems?
+# $ffmpeg = ffprobe -i /home/david/Videos/furiadeloskaratecas1983.mp4 2>&1
+
+# Sample output of ffprobe data provided. Uncomment above, if you have the ffmpeg suite, and comment out below.
+$ffmpeg = @"
+ffprobe version 4.1.4-1+rpt1~deb10u1 Copyright (c) 2007-2019 the FFmpeg developers
+  built with gcc 8 (Raspbian 8.3.0-6+rpi1)
+  configuration: --prefix=/usr --extra-version='1+rpt1~deb10u1' --toolchain=hardened --libdir=/usr/lib/arm-linux-gnueabihf --incdir=/usr/include/arm-linux-gnueabihf --arch=arm --enable-gpl --disable-stripping --enable-avresample --disable-filter=resample --enable-avisynth --enable-gnutls --enable-ladspa --enable-libaom --enable-libass --enable-libbluray --enable-libbs2b --enable-libcaca --enable-libcdio --enable-libcodec2 --enable-libflite --enable-libfontconfig --enable-libfreetype --enable-libfribidi --enable-libgme --enable-libgsm --enable-libjack --enable-libmp3lame --enable-libmysofa --enable-libopenjpeg --enable-libopenmpt --enable-libopus --enable-libpulse --enable-librsvg --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libssh --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvorbis --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx265 --enable-libxml2 --enable-libxvid --enable-libzmq --enable-libzvbi --enable-lv2 --enable-omx --enable-openal --enable-opengl --enable-sdl2 --enable-omx-rpi --enable-mmal --enable-libdc1394 --enable-libdrm --enable-libiec61883 --enable-chromaprint --enable-frei0r --enable-libx264 --enable-shared
+  libavutil      56. 22.100 / 56. 22.100
+  libavcodec     58. 35.100 / 58. 35.100
+  libavformat    58. 20.100 / 58. 20.100
+  libavdevice    58.  5.100 / 58.  5.100
+  libavfilter     7. 40.101 /  7. 40.101
+  libavresample   4.  0.  0 /  4.  0.  0
+  libswscale      5.  3.100 /  5.  3.100
+  libswresample   3.  3.100 /  3.  3.100
+  libpostproc    55.  3.100 / 55.  3.100
+Input #0, mov,mp4,m4a,3gp,3g2,mj2, from 'furiadeloskaratecas1983.mp4':
+  Metadata:
+    major_brand     : mp42
+    minor_version   : 0
+    compatible_brands: isommp42
+    creation_time   : 2015-11-27T06:39:43.000000Z
+  Duration: 01:25:56.97, start: 0.000000, bitrate: 1590 kb/s
+    Stream #0:0(und): Video: h264 (High) (avc1 / 0x31637661), yuv420p, 1280x720 [SAR 1:1 DAR 16:9], 1395 kb/s, 25 fps, 25 tbr, 25 tbn, 50 tbc (default)
+    Metadata:
+      handler_name    : VideoHandler
+    Stream #0:1(und): Audio: aac (LC) (mp4a / 0x6134706D), 44100 Hz, stereo, fltp, 192 kb/s (default)
+    Metadata:
+      creation_time   : 2015-11-27T06:40:23.000000Z
+      handler_name    : IsoMedia File Produced by Google, 5-11-2011
+"@
+
+$ffmpeg = $ffmpeg -split ("`n")
+
+[video]$ThisVideo = @{
+    "FileName" = $((Get-ChildItem $Filename).ToString())
+    "MajorBrand" = $null
+    "MinorVersion" = $null
+    "CompatibleBrands" = $null
+    "CreationTime" = $null
+    "Duration" = $null
+    "Start" = $null
+    "BitRate" = $null
+    "VideoStream" = $null
+    "AudioStream" = $null
+    "HandlerName" = $null
+}
+
+$ffmpeg.ForEach({
+    if($PSItem -match "major_brand"){
+        $ThisVideo.MajorBrand = $($PSItem.Trim() -replace "major_brand     : ","")
+    }
+    if($PSItem -match "minor_version"){
+        $ThisVideo.MinorVersion = $($PSItem.Trim() -replace "minor_version   : ","")
+    }
+    if($PSItem -match "compatible_brands"){
+        $ThisVideo.CompatibleBrands = $($PSItem.Trim() -replace "compatible_brands: ","")
+    }
+
+    if($PSItem -match "handler_name"){
+        $ThisVideo.HandlerName += $($PSItem.Trim() -replace "handler_name    : ","")
+    }
+    #Only capture the first creation time, until streams are better understood
+    if($PSItem -match "creation_time" -and $ThisVideo.CreationTime -eq ""){
+        $ThisVideo.CreationTime = $($PSItem.Trim() -replace "creation_time   : ","")
+    }
+    if($PSItem -match "Duration"){
+        $Duration = ([regex]::Match($PSItem,"Duration: [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{1,}")).Value
+        $Duration = $Duration -replace "Duration: ",""
+        $Duration = $Duration -replace "\.",":"
+        $Duration = $Duration -split ":"
+        $Duration = @{
+            "Hours" = $Duration[0]
+            "Minutes" = $Duration[1]
+            "Seconds" = $Duration[2]
+            "Milliseconds" = $Duration[3]
+        }
+        $ThisVideo.Duration = $Duration
+    }
+    if($PSItem -match "start"){
+        $Start = ([regex]::Match($PSItem,"start: [0-9]{1,}\.[0-9]{1,6}")).Value
+        $Start = $Start -replace "start: ",""
+        $ThisVideo.Start = $Start
+    }
+    if($PSItem -match "bitrate"){
+        $BitRate = ([regex]::Match($PSItem,"bitrate: [0-9]{1,4}\s[a-z]b/s")).Value
+        $BitRate = $BitRate -replace "bitrate: ",""
+        $ThisVideo.BitRate = $BitRate
+    }
+    if($PSItem -match "Video: "){
+        $VideoStream = ([regex]::Match($PSItem,"Video: .{1,}$")).Value
+        $VideoStream = $VideoStream -replace "Video: ",""
+        $VideoStream = $VideoStream -split ","
+        $ThisVideo.VideoStream = $VideoStream
+    }
+    if($PSItem -match "Audio: "){
+        $AudioStream = ([regex]::Match($PSItem,"Audio: .{1,}$")).Value
+        $AudioStream = $AudioStream -replace "Audio: ",""
+        $AudioStream = $AudioStream -split ","
+        $ThisVideo.AudioStream = $AudioStream
+    }
+})
